78c977209a6b952705ae9eb2bdc26423885370d3
Aida Colovic <aida.colovic.ffm@gmail.com>
2022-01-26 21:50:18 +0100

    add practice session of winter contest 2022

---
+++ wintercontest2022/practice/probB.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+int main(void)
+{
+    long long a;
+    std::cin >> a;
+    double res = 6 * std::sqrt((a*2)/(3*std::sqrt(3)));
+    std::cout << std::fixed << std::setprecision(8) << res << std::endl;
+    return 0;
+}
