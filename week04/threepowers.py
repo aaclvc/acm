d46ff104e7343cfdf0c227ff8e8fee194b92e6a8
Aida Colovic <aida.colovic.ffm@gmail.com>
2022-02-05 23:58:51 +0100

    :pencil2: add comment header

--- week04/threepowers.py
+++ week04/threepowers.py
@@ -1,3 +1,10 @@
+#  Aida Colovic
+#  Kattis Problem   : Three Powers
+#  Problem ID       : threepowers
+#  CPU Time limit   : 1 sec
+#  Memory limit     : 1024 MB
+#  Difficultiy      : 2.3
+
 def solve(n):
     begin = True
     shift = 0
